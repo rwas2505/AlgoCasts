2cddb90af1e54bb907bc2bf21d856ca57e0c0726
Ryan <ryanwaszak@gmail.com>
2021-12-19 00:09:00 -0600

    Adds maxchar solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  var dict = {};
+
+  for (i=0; i < str.length; i++){
+    if (str[i] in dict){
+      dict[str[i]] += 1
+    } else{
+      dict[str[i]] = 1
+    }
+  }
+
+  let max = 0;
+  let maxKey = "";
+
+  for(let char in dict){
+    if (dict[char] > max){
+      max = dict[char];
+      maxKey = char;
+    };
+  }
+
+  return maxKey;
+}
 
 module.exports = maxChar;
