6c44f98d9e88ef77ff11a7b4a364498f4a8b6db1
Ryan <ryanwaszak@gmail.com>
2021-12-22 00:20:42 -0600

    Solves anagrams before clean refactor

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,47 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  var dictA = {};
+var dictB = {};
+  
+  var sanitizedStringA = stringA.replace(/[^\w]/g, "").toLowerCase();
+
+  var sanitizedStringB = stringB.replace(/[^\w]/g, "").toLowerCase();
+
+   for (i=0; i < sanitizedStringA.length; i++){
+    if (sanitizedStringA[i] in dictA){
+      dictA[sanitizedStringA[i]] += 1
+    } else{
+      dictA[sanitizedStringA[i]] = 1
+    }
+  }
+
+  for (i=0; i < sanitizedStringB.length; i++){
+    if (sanitizedStringB[i] in dictB){
+      dictB[sanitizedStringB[i]] += 1
+    } else{
+      dictB[sanitizedStringB[i]] = 1
+    }
+  }
+
+  var lengthA = Object.keys(dictA).length;
+  var lengthB = Object.keys(dictB).length;
+
+  var validLengths = lengthA === lengthB;
+
+  const test = {a: 1, b: 2, c: 3};
+  
+var result = true;
+  
+for (const [key, value] of Object.entries(dictA)) {
+
+  if (dictB[key] != value){
+    result = false;
+  }
+}
+
+  return result && validLengths;
+}
 
 module.exports = anagrams;
