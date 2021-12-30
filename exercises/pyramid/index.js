87d6bcc39c461906617a2ac294376a96c76737c4
Ryan Waszak <rwaszak@tesla.com>
2021-12-29 17:38:39 -0800

    Adds pyramid solution with recursion

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,23 +14,44 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-  var colCount = n * 2 - 1;
-  var rowCount = n;
-
-  for (row = 0; row < rowCount; row ++) {
-    var level = '';
-    for (col = 0; col < colCount; col++){
-      if (col === n-1){
-        level += "#";
-      } else if (col <= n-1+row && col >= n-1-row){
-        level += "#"
-      } else {
-        level += " ";
-      }
-    }
-    console.log(level)  
+function pyramid(n, row = 0, level='') {
+  if(row === n) {
+    return;
   }
+
+  if (level.length === 2 * n -1){
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor((2 * n -1) / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length){
+    add = "#";
+  } else{
+    add = " ";
+  }
+
+  pyramid(n, row, level + add);
 }
 
 module.exports = pyramid;
+
+// function pyramid(n) {
+//   var colCount = n * 2 - 1;
+//   var rowCount = n;
+
+//   for (row = 0; row < rowCount; row ++) {
+//     var level = '';
+//     for (col = 0; col < colCount; col++){
+//       if (col === n-1){
+//         level += "#";
+//       } else if (col <= n-1+row && col >= n-1-row){
+//         level += "#"
+//       } else {
+//         level += " ";
+//       }
+//     }
+//     console.log(level)  
+//   }
+// }
